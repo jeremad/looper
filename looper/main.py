46b1a21d373ef27dea742126d120584e3878d744
jerem <jeremy.tellaa@tanker.io>
2019-02-15 11:16:28 +0100

    bootstrap

---
+++ looper/main.py
@@ -0,0 +1,52 @@
+import argparse
+import subprocess
+import sys
+
+import cli_ui as ui
+from typing import List, Optional
+
+ArgsList = Optional[List[str]]
+
+def run(cmd: str) -> int:
+    ui.info_2(cmd)
+    cmd = cmd.split()
+    process = subprocess.run(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.returncode:
+        ui.info_1(process.stdout.decode("utf-8"))
+        ui.error(process.stderr.decode("utf-8"))
+    return process.returncode
+
+
+def loop(cmd: List, max_tries: int, stop_on_first_fail: bool) -> None:
+    if not cmd or not cmd[0]:
+        ui.error("no command provided")
+        sys.exit(1)
+    cmd = cmd[0]
+    runs = 0
+    fails = 0
+    while True:
+        if run(cmd):
+            if stop_on_first_fail:
+                return
+            fails += 1
+        runs += 1
+        if max_tries and runs >= max_tries:
+            ui.info_1(f"command \"{cmd}\" failed {fails} times after {max_tries} tries")
+            return
+
+
+
+def main(args: ArgsList = None) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "cmd", nargs=1, type=str,
+        help="The command you want to run in a loop, between double quotes")
+    parser.add_argument(
+        "-m", "--max-tries", type=int, default=100,
+        help="Maximum number of time running the command, 0 means no limit")
+    parser.add_argument(
+        "-s", "--stop-on-first-fail", action="store_true",
+        help="If set looper will stop on the first fail")
+    args_ns = parser.parse_args(args=args)
+    loop(args_ns.cmd, args_ns.max_tries, args_ns.stop_on_first_fail)
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info.major < 3:
+    sys.exit("Error: Please upgrade to Python3")
+
+def get_long_description():
+    with open("README.md") as fp:
+        return fp.read()
+
+setup(
+    name="py-loop",
+    version="0.1.0",
+    description="Run commands until it fails",
+    long_description=get_long_description(),
+    url="https://github.com/jeremad/looper",
+    author="Kontrol SAS",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=[
+        "cli-ui",
+    ],
+    entry_points={"console_scripts": ["looper = looper.main:main"]},
+)
