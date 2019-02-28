aabcd88c9e352c91be07c83320b74c4559ba255a
jerem <jeremy.tellaa@tanker.io>
2019-02-28 16:11:36 +0100

    add tests for the main

---
+++ looper/tests/test_main.py
@@ -0,0 +1,17 @@
+import looper.main
+
+import pytest
+
+
+def test_version():
+    assert looper.main.main(["--version"]) is None
+
+
+def test_no_cmd():
+    with pytest.raises(SystemExit) as e:
+        looper.main.main([""])
+    assert e.value.code == 1
+
+
+def test_cmd():
+    assert looper.main.main(["ls", "--max-tries", "1"]) is None
