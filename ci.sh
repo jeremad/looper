d35c32ea31f6633935ee16d6081ad661a53e696b
jerem <jeremy.tellaa@tanker.io>
2019-11-26 18:06:30 +0100

    feat(ci): use github actions

--- ci.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -x
-set -e
-
-dmenv run -- black --check looper
-dmenv run flake8 .
-dmenv run -- mypy --ignore-missing-imports --strict .
-dmenv run -- pytest --cov=looper --cov-report=xml --cov-report=term
-dmenv run -- python-codacy-coverage -r coverage.xml
