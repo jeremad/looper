f325a1f11a1f36907d86efbee274e0dd16fd0ca8
jerem <jeremy.tellaa@tanker.io>
2022-01-25 16:18:09 +0100

    chore: update python deps

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,16 +3,16 @@ import py_loop.main as looper_main
 import pytest
 
 
-def test_version():
-    assert looper_main.main(["--version"]) is None
+def test_version() -> None:
+    looper_main.main(["--version"])
 
 
-def test_no_cmd():
+def test_no_cmd() -> None:
     with pytest.raises(SystemExit) as e:
         looper_main.main([""])
     assert e.value.code == 1
 
 
-def test_cmd():
-    assert looper_main.main(["ls", "--max-tries", "1"]) is None
-    assert looper_main.main(["--max-tries", "1", "ls"]) is None
+def test_cmd() -> None:
+    looper_main.main(["ls", "--max-tries", "1"])
+    looper_main.main(["--max-tries", "1", "ls"])
